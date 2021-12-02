462b790ed427c67e1f32ed9824b71858e53e60c0
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2021-12-02 13:34:20 +0530

    Setup project & Connect DB

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require("express-ejs-layouts");
+var cors = require('cors');
+const db = require("./config/mongoose");
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require("connect-mongo");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
+app.use(expressLayouts);
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,6 +31,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// // mongo strore is used to store session cookie
+app.use(
+  session({
+    secret: "blahblah",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb://localhost/ie_local_store"
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
