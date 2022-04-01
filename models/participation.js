c825ec52c1fffff028e6d9108fc42c6d9a5ac751
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2022-04-01 12:28:33 +0530

    Upload and view Proof

--- models/participation.js
+++ models/participation.js
@@ -1,50 +1,77 @@
 const mongoose = require("mongoose");
 
-const participationSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    collegeId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "College",
-      required: true,
-    },
-    // eventId: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "Event",
-    //   required: true,
-    // },
-    eventname: {
-      type: String,
-      required: true,
-    },
-    eventtype: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    dates: [
-      {
-        type: Date,
-      },
-    ],
-    applicationStatus: {
-      type: String,
-    },
-    proofOfParticipation: {
-      type: String,
-    },
+const multer = require("multer");
+const path = require("path");
+const PROOF_PATH = path.join("/uploads/proofs/");
+
+const participationSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
-  {
-    timestamps: true,
-  }
+  // collegeId: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "College",
+  //   required: true,
+  // },
+  // eventId: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "Event",
+  //   required: true,
+  // },
+  collegename: {
+    type: String,
+    required: true,
+  },
+  eventname: {
+    type: String,
+    required: true,
+  },
+  eventtype: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  dates: [{
+    type: Date,
+  }, ],
+  applicationStatus: {
+    type: String,
+  },
+  proofOfParticipation: {
+    type: String,
+  },
+}, {
+  timestamps: true,
+});
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../publc", PROOF_PATH));
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      // req.body.name
+      // +
+      //   "-" +
+      Date.now() +
+      path.extname(file.originalname)
+    );
+  },
+});
+
+// static methods
+participationSchema.statics.uploadedProof = multer({
+  storage: storage
+}).single(
+  "proofOfParticipation"
 );
+participationSchema.statics.proofPath = PROOF_PATH;
 
 const Participation = mongoose.model("Participation", participationSchema);
 
--- routes/student.js
+++ routes/student.js
@@ -3,38 +3,60 @@ var router = express.Router();
 var Participation = require("../models/participation");
 var College = require("../models/college");
 
-router.get("/", function (req, res) {
-  return res.render("student");
-});
-router.get("/request-participation", async function (req, res) {
-  var colleges = await College.find({}).sort({ collegename: 1 });
-  return res.render("requestparticipation", { colleges });
-});
-router.post("/request-participation", function (req, res) {
-
-  College.find({collegename: req.body.collegename}, function (err, colleges) {
-    console.log(colleges);
-    req.body.userId = req.user._id;
-    req.body.collegeId = colleges[0]._id;
-    req.body.applicationStatus = "pending";
-    var dates = [];
-    dates.push(req.body.date1);
-    if (req.body.date2) dates.push(req.body.date2);
-    if (req.body.date3) dates.push(req.body.date3);
-    req.body.dates = dates;
-    Participation.create(req.body, function (err, participation) {
-      if (err) {
-        console.log("Error in requesting participation: ", err);
-        return;
-      }
-      console.log("Participation: ", participation);
-      return res.redirect("/student/activities");
+// router.get("/", function (req, res) {
+//   return res.render("student");
+// });
+router.get("/", function (req, res, next) {
+  Participation.find({
+    userId: req.user._id
+  }, function (err, participations) {
+    return res.render("activities", {
+      participations
     });
   });
 });
-router.get("/activities", function (req, res, next) {
-  Participation.find({ userId: req.user._id }, function (err, participations) {
-    return res.render("activities", { participations });
+router.get("/request-participation", async function (req, res) {
+  var colleges = await College.find({}).sort({
+    collegename: 1
+  });
+  return res.render("requestparticipation", {
+    colleges
+  });
+});
+router.post("/request-participation", function (req, res) {
+  req.body.userId = req.user._id;
+  req.body.applicationStatus = "pending";
+  var dates = [];
+  dates.push(req.body.date1);
+  if (req.body.date2) dates.push(req.body.date2);
+  if (req.body.date3) dates.push(req.body.date3);
+  req.body.dates = dates;
+  Participation.create(req.body, function (err, participation) {
+    if (err) {
+      console.log("Error in requesting participation: ", err);
+      return;
+    }
+    console.log("Participation: ", participation);
+    return res.redirect("/student/activities");
+  });
+});
+router.post("/upload-proof/:participationId", function (req, res) {
+  Participation.uploadedProof(req, res, function (err) {
+    console.log(">>> body: ", req.body)
+    Participation.findByIdAndUpdate(
+      req.params.participationId, {
+        proofOfParticipation: Participation.proofPath + req.file.filename
+      },
+      function (err, participation) {
+        if (err) {
+          console.log("Error on uploading prrof: ", err);
+        }
+        if (participation) {
+          console.log("Uploaded: ", participation);
+          return res.redirect("/student");
+        }
+      }
+    );
   });
 });
 module.exports = router;
